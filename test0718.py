93cd6ffe9ca8d29d86ee57b9ac36d467f1f358f3
SunnyHsu <kuochang209@gmail.com>
2023-07-19 17:36:35 +0800

    update

--- test0718.py
+++ test0718.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib as plt
+import plotly.express as px
 
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
@@ -18,52 +20,7 @@ st.title('Uber pickups in NYC')
 
 st.snow()
 
-DATA_URL = "C:\\Users\\AXTWN3V\\Desktop\\Sunny Hsu\\Reviews Analysis Project\\20230717055154221d.csv"
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    return data
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10)
-data
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done ! (using st.cache_data)")
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-    
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
-   
-with st.container():
-   st.write("This is inside the container")
-
-   # You can call any Streamlit command, including custom components:
-   st.bar_chart(np.random.randn(50, 3))
     
