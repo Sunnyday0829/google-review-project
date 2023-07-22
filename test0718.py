3aa65c0c869d5f9ca05bbb15f0ed1896cd4654c9
SunnyHsu <kuochang209@gmail.com>
2023-07-22 15:53:50 +0800

    update

--- test0718.py
+++ test0718.py
@@ -4,23 +4,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+### ========== ###
+
+# Page Configuration
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
+    page_title = "Audi Customer Reviews Dashboard",
+    page_icon = "./my_icon.png",
+    layout = "wide", # centered/wide
+    initial_sidebar_state = "auto", # auto/expanded/collapsed
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
+        'About': "Established by Sunny Hsu 2023 Summer"
     }
 )
 
-st.title('Uber pickups in NYC')
+# Page Title
+st.title(':red_car: Audi Customer Reviews Dashboard')
 
-# 載入資料
+### ========== ###
+
+# Read Data
 data_outscraper = pd.read_csv("./20230717055154221d.csv")
 data = data_outscraper[['name','reviews','rating','review_text','owner_answer','owner_answer_timestamp_datetime_utc','review_rating','review_datetime_utc','review_likes']]
+
+### ========== ###
+
+# Page Sidebar
+with st.sidebar:
+    selectbox_value = st.multiselect(
+        "Choose a Location",
+        data['name'].unique()
+    )
+
+### ========== ###
+
+# Filtered Data
+data = data[data['name'].isin(selectbox_value)]
 
 # 展示 DataFrame
 st.dataframe(data.head(n = 5))
@@ -28,17 +46,53 @@ st.dataframe(data.head(n = 5))
 # 顯示資料型態
 st.text(data.dtypes)
 
-#
+# 表格
 data_ratings = data.groupby(by = "name")['rating'].mean().reset_index()
 st.dataframe(data_ratings)
 
+data_rating_percentage = data.sort_values('review_rating').groupby(['name','review_rating'])['review_rating'].count().reset_index(name = 'count')
+st.dataframe(data_rating_percentage)
+
 # 畫圖
-fig = px.bar(data_ratings, x = "rating", y = "name", orientation = 'h')
+fig_ratings = px.bar(data_ratings, 
+             x = "rating", y = "name", 
+             orientation = 'h',
+             title = "Overall Ratings",
+             text_auto = True,
+             color = 'rating', color_continuous_scale=['#bcbcbc', '#de0909'])
+fig_ratings.update_traces(textfont_size = 12, textangle = 0, textposition = 'outside', cliponaxis = False)
+fig_ratings.update_layout(plot_bgcolor = 'white', 
+                  xaxis_title = 'Ratings', yaxis_title = "Location",
+                  font = dict(size = 12, color = 'black'),
+                  coloraxis_showscale = False)
 
-# 展示圖形
-st.plotly_chart(fig)
 
+fig_rating_percentage = px.histogram(data_rating_percentage, x = "name",
+                    y = "count", color = "review_rating",
+                    barnorm = 'percent', text_auto = True,
+                    title = "Past 3 Months Ratings Percentage",
+                    category_orders={"review_rating" : [1,2,3,4,5]},
+                    color_discrete_map={1:'#ffbaba', 2:'#ff7b7b', 3:'#ff5252', 4:'#ff0000', 5:'#a70000'})
+fig_rating_percentage.update_traces(textfont_size = 12, textangle = 0, textposition = 'inside', cliponaxis = False)
+fig_rating_percentage.update_layout(plot_bgcolor = 'white', 
+                  xaxis_title = 'Location', yaxis_title = "Percentage",
+                  font = dict(size = 12, color = 'black'))
 
+### ========== ###
 
+# Construct Tabs and Columns for Page Layout
+tab1, tab2 = st.tabs(["Overview", "Highlighted Reviews"])
 
+with tab1:
+
+    st.plotly_chart(fig_ratings,
+                    use_container_width = True)
+    st.plotly_chart(fig_rating_percentage,
+                    use_container_width = True)
+
+with tab2:
+
+    st.subheader("Highlighted Reviews")
+    st.dataframe(data_ratings)
+    st.dataframe(data_rating_percentage)
     
